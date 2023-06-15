78de32f546bf1dab41b940cc8a6e27b7bdf568fe
dagon12345 <lanceespina478@gmail.com>
2023-06-16 07:55:17 +0800

    Main

--- Queuing_System/Properties/AssemblyInfo.cs
+++ Queuing_System/Properties/AssemblyInfo.cs
@@ -33,11 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-<<<<<<< Updated upstream
+
 [assembly: AssemblyVersion("1.9.0.0")]
 [assembly: AssemblyFileVersion("1.9.0.0")]
-=======
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
->>>>>>> Stashed changes
-[assembly: NeutralResourcesLanguage("en")]
+
