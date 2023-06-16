dd4763dff75a99990d12b4ef0ff802dada3ec340
dagon12345 <lanceespina478@gmail.com>
2023-06-16 08:19:40 +0800

    commit

--- Queuing_System/frmOption.cs
+++ Queuing_System/frmOption.cs
@@ -21,36 +21,7 @@ namespace Queuing_System
             InitializeComponent();
 
 
-            
-            WebClient webClient = new WebClient();
-            var client = new WebClient();
-
-            if (!webClient.DownloadString("https://www.dropbox.com/s/62hfq7ylzn480sv/Update.txt?dl=1").Contains("1.9.0"))
-            {
-                if (MessageBox.Show("New update available! Do you want to install it?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    try
-                    {
-                        if (File.Exists(@".\QueueInstaller.msi")) { File.Delete(@".\QueueInstaller.msi"); }
-                        client.DownloadFile("https://www.dropbox.com/s/pa8ydxmxgud2926/QueueInstaller.zip?dl=1", @"QueueInstaller.zip");
-                        string zipPath = @".\QueueInstaller.zip";
-                        string extractPath = @".\";
-                        ZipFile.ExtractToDirectory(zipPath, extractPath);
-
-                        Process process = new Process();
-                        process.StartInfo.FileName = "msiexec";
-                        process.StartInfo.Arguments = String.Format("/i QueueInstaller.msi");
-
-                        this.Close();
-                        process.Start();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-                }
-            }
-
+        
 
             
 
@@ -64,6 +35,41 @@ namespace Queuing_System
                 {
 
                     lbl_internet.Text = "Online";
+
+
+                    ////////////CHECK UPDATES
+
+                    WebClient webClient = new WebClient();
+                    var client = new WebClient();
+
+                    if (!webClient.DownloadString("https://www.dropbox.com/s/62hfq7ylzn480sv/Update.txt?dl=1").Contains("1.9.0"))
+                    {
+                        if (MessageBox.Show("New update available! Do you want to install it?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            try
+                            {
+                                if (File.Exists(@".\QueueInstaller.msi")) { File.Delete(@".\QueueInstaller.msi"); }
+                                client.DownloadFile("https://www.dropbox.com/s/pa8ydxmxgud2926/QueueInstaller.zip?dl=1", @"QueueInstaller.zip");
+                                string zipPath = @".\QueueInstaller.zip";
+                                string extractPath = @".\";
+                                ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+                                Process process = new Process();
+                                process.StartInfo.FileName = "msiexec";
+                                process.StartInfo.Arguments = String.Format("/i QueueInstaller.msi");
+
+                                this.Close();
+                                process.Start();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                        }
+                    }
+
+
+
 
                 }
 
@@ -95,43 +101,7 @@ namespace Queuing_System
 
         private void Option_Load(object sender, EventArgs e)
         {
-            /*
-            WebClient webClient = new WebClient();
-            var client = new WebClient();
-
-            if (!webClient.DownloadString("https://www.dropbox.com/s/62hfq7ylzn480sv/Update.txt?dl=1").Contains("2.3.0"))
-            {
-                if (MessageBox.Show("New update available! Do you want to install it?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    try
-                    {
-                        if (File.Exists(@".\Installer.msi")) { File.Delete(@".\Installer.msi"); }
-                        client.DownloadFile("https://www.dropbox.com/s/rj5paxa5lnvjggy/Installer.zip?dl=1", @"Installer.zip");
-                        string zipPath = @".\Installer.zip";
-                        string extractPath = @".\";
-                        ZipFile.ExtractToDirectory(zipPath, extractPath);
-
-                        Process process = new Process();
-                        process.StartInfo.FileName = "msiexec";
-                        process.StartInfo.Arguments = String.Format("/i Installer.msi");
-
-                        this.Close();
-                        process.Start();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                        this.Close();
-                    }
-                }
-                else
-                {
-
-                }
-
-
-            }
-            */
+              
 
 
 
