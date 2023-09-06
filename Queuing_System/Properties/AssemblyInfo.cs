e0049ccc26bb65f792751598914221a02f603d0a
dagon12345 <lanceespina478@gmail.com>
2023-09-06 17:39:53 +0800

    EW

--- Queuing_System/Properties/AssemblyInfo.cs
+++ Queuing_System/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.7.6")]
-[assembly: AssemblyFileVersion("3.7.6")]
+[assembly: AssemblyVersion("3.7.7")]
+[assembly: AssemblyFileVersion("3.7.7")]
 
